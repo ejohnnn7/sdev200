d035cd6039cfe37fd66f3a5b9a5e09b69a940562
Elizabeth John <ejohnnn7@gmail.com>
2025-08-30 18:58:26 -0400

    Chapter 6:  Programming Exercise 6.31

---
+++ CreditCardValidator.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class CreditCardValidator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a credit card number: ");
+        long number = input.nextLong();
+
+        if (isValid(number)) {
+            System.out.println(number + " is a valid credit card number.");
+        } else {
+            System.out.println(number + " is not a valid credit card number.");
+        }
+    }
+
+    /** Return true if the card number is valid */
+    public static boolean isValid(long number) {
+        int size = getSize(number);
+        if (size < 13 || size > 16) {
+            return false;
+        }
+
+        if (!(prefixMatched(number, 4) || prefixMatched(number, 5) ||
+              prefixMatched(number, 37) || prefixMatched(number, 6))) {
+            return false;
+        }
+        int sum = sumOfDoubleEvenPlace(number) + sumOfOddPlace(number);
+        return sum % 10 == 0;
+    }
+
+    /** Get the result from Step 2 */
+    public static int sumOfDoubleEvenPlace(long number) {
+        String numStr = Long.toString(number);
+        int sum = 0;
+        for (int i = numStr.length() - 2; i >= 0; i -= 2) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            sum += getDigit(digit * 2);
+        }
+        return sum;
+    }
+
+    /** Return this number if it is a single digit, otherwise, return the sum of the two digits */
+    public static int getDigit(int number) {
+        if (number < 10) {
+            return number;
+        }
+        return number / 10 + number % 10;
+    }
+
+    /** Return sum of odd-place digits in number */
+    public static int sumOfOddPlace(long number) {
+        String numStr = Long.toString(number);
+        int sum = 0;
+
+        for (int i = numStr.length() - 1; i >= 0; i -= 2) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            sum += digit;
+        }
+
+        return sum;
+    }
+
+    /** Return true if the number d is a prefix for number */
+    public static boolean prefixMatched(long number, int d) {
+        return getPrefix(number, getSize(d)) == d;
+    }
+
+    /** Return the number of digits in d */
+    public static int getSize(long d) {
+        return Long.toString(d).length();
+    }
+
+    /** Return the first k number of digits from number. If the number of digits in number is less than k, return number. */
+    public static long getPrefix(long number, int k) {
+        String numStr = Long.toString(number);
+        if (numStr.length() < k) {
+            return number;
+        }
+        return Long.parseLong(numStr.substring(0, k));
+    }
+}
